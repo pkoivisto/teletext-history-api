8ad18cbe323bdfcd203b6f6e8587668baeada168
Pyry Koivisto <pyry.koivisto@iki.fi>
2021-09-04 20:36:26 +0300

    Abstract communication with API with a protocol, define an implementation that uses stored credentials to communicate with the API

--- src/teletext_history_api/image_downloader.clj
+++ src/teletext_history_api/image_downloader.clj
@@ -1,18 +1,52 @@
 (ns teletext-history-api.image-downloader
   (:require [clj-http.client :as http]))
 
-(defn- page+subpage->url [page subpage]
+(defn- image-url [page subpage]
   (str "https://external.api.yle.fi/v1/teletext/images/" page "/" subpage ".png"))
 
-(defn get-image [{:keys [page subpage timestamp app_id app_key] :as m}]
+(defn- page-url [page]
+  (str "https://external.api.yle.fi/v1/teletext/pages/" page ".json"))
+
+(defn- get-image [{:keys [page subpage app_id app_key]}]
   {:pre [(string? page)
          (string? subpage)
          (string? app_id)
          (string? app_key)]}
-  ;(clojure.pprint/pprint m)
-  (let [url-to-get (page+subpage->url page subpage)
+  (let [url (image-url page subpage)
         opts {:query-params {:app_id  app_id
                              :app_key app_key}
-              :as :byte-array}]
-    (http/get url-to-get opts)))
+              :as           :byte-array}
+        response (http/get url opts)]
+    (case (:status response)
+      200 (:body response)
+      nil)))
+
+(defn- get-page [{:keys [page app_id app_key]}]
+  {:pre [(string? page)
+         (string? app_id)
+         (string? app_key)]}
+  (let [url (page-url page)
+        opts {:query-params {:app_id  app_id
+                             :app_key app_key}}
+        response (http/get url opts)]
+    (case (:status response)
+      200 (:body response)
+      nil)))
+
+(defprotocol APIClient
+  (get-image-png [this page subpage])
+  (get-page-json [this page]))
+
+(defrecord APIClientImpl
+  [app_key app_id]
+  APIClient
+  (get-image-png [_ page subpage]
+    (get-image {:app_key app_key
+                :app_id  app_id
+                :page    page
+                :subpage subpage}))
+  (get-page-json [_ page]
+    (get-page {:app_key app_key
+               :app_id  app_id
+               :page    page})))
 
